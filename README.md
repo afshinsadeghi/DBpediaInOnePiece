501cbc06b08069a4664e87d22527ef8dd4a730bd
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2018-03-26 17:34:20 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DBPediaDownloder
-This code downloads and extracts and merges into one file the last version of DBPedia.
+This code downloads and extracts and merges all ttl files of the current version of DBpedia into one file
+ with name dbpedia.ttl
+
+
+## Please make sure first
+
+ 1. You at least have > 30 GB of free disk space on your disk
+
+ 2. You have bzip2 installed on your system
+
+ 3. Your python has the packages:  "mechanize" "subprocess" ,"time"
+
+
+## Questions? find me @:
+
+http://afshn.com
+
+http://sda.cs.uni-bonn.de/people/afshin-sadeghi/
+
+https://scholar.google.com/citations?user=uWTszVEAAAAJ&hl=en&oi=ao
+
---
+++ dbpediaDown.py
@@ -0,0 +1,29 @@
+import mechanize
+from subprocess import call
+from time import sleep
+br = mechanize.Browser()
+
+br.open('http://downloads.dbpedia.org/current/core-i18n/en/')
+
+f=open("source.html","w")
+f.write(br.response().read())
+
+filetypes=[".ttl.bz2"]
+myfiles=[]
+for l in br.links():
+    for t in filetypes:
+        if t in str(l):
+            myfiles.append(l)
+
+
+def downloadlink(l):
+    f=open(l.text,"w")
+    br.click_link(l)
+    f.write(br.response().read())
+    print l.text," is downloaded."
+    call(["bzip2 - d", l.tex])
+
+
+for l in myfiles:
+    sleep(1) # sleep so to let the server breath
+    downloadlink(l)
