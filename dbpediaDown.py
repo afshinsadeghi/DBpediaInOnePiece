4e3654e2ecfd1ed498eb4e4eb650c6fff8045ade
Afshin Sadeghi <sadeghi@cs.uni-bonn.de>
2018-03-26 17:48:56 +0200

    second commit

--- dbpediaDown.py
+++ dbpediaDown.py
@@ -20,8 +20,8 @@ def downloadlink(l):
     f=open(l.text,"w")
     br.click_link(l)
     f.write(br.response().read())
-    print l.text," is downloaded."
-    call(["bzip2 - d", l.tex])
+    print l.text," is downloaded. Extracting..."
+    call(["bzip2 - d", l.text])
 
 
 for l in myfiles:
@@ -29,6 +29,9 @@ for l in myfiles:
     downloadlink(l)
 
 call(["rm *.bz2"])
+print "removing downloaded files..."
 call(["cat *.ttl > a.ttt"])
+print "merging..."
 call(["rm *.ttl"])
 call(["mv a.ttt DBpedia.ttl"])
+print "DBpedia.ttl  is created. have fun!"
